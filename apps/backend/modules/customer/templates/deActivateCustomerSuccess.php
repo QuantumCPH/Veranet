651d00a641bfe7c1538b58e4ba188402b49b48f3
Fatima Umer <fu@zerocall.com>
2012-07-20 15:47:14 +0500

    css

--- apps/backend/modules/customer/templates/deActivateCustomerSuccess.php
+++ apps/backend/modules/customer/templates/deActivateCustomerSuccess.php
@@ -1,28 +1,23 @@
-<?php
-/* 
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
-?><div id="sf_admin_container">
-<div class="sf_admin_filters">
-    <fieldset>  
-    <h1 style="margin-top: 0;">De-Activate Customer</h1><br />
-<form action="" method="get">
-    <div class="form-row">
-             <label for=""><strong>Customer Id:</strong></label>
-    <div class="content">
-             <input type="text" name="customer_id" />
+<div id="sf_admin_container">
+    <div class="sf_admin_filters">
+        <h1>De-Activate Customer</h1>
+        <form action="" method="get">
+            <fieldset>
+                <div class="form-row">
+                    <label>Customer Id:</label>
+                    <div class="content">
+                        <input type="text" name="customer_id" />
+                    </div>
+                </div>
+                <div class="form-row">
+                    <?php echo $response_text ?>
+                </div>
+             </fieldset>
+            <ul class="sf_admin_actions">
+                <li>
+                    <input type="submit" value="De-Activate" class="user_external_link" />
+                </li>
+          </ul>
+        </form>
     </div>
-    </div>
-    <div class="form-row">
-            <div class="content">
-              <input type="submit" value="De-Activate" class="user_external_link" />
-            </div>
-    </div>
-</form>
-
-<br/>
-<?php echo $response_text ?>
-<br />
 </div>
