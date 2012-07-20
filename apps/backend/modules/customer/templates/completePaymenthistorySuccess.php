b6d504afc7a388f41585ecba76298f362d172a17
Fatima Umer <fu@zerocall.com>
2012-07-20 11:51:55 +0500

    css

--- apps/backend/modules/customer/templates/completePaymenthistorySuccess.php
+++ apps/backend/modules/customer/templates/completePaymenthistorySuccess.php
@@ -33,64 +33,51 @@
            <li><input type="submit" name="Search" value="Search" class="sf_admin_action_filter" /></li>
         </ul>
     </form>
-</div>
-
-<div id="sf_admin_container">
-    
+ 
     <table width="100%" cellspacing="0" cellpadding="2" class="callhistory tblAlign">
-    
-    <tr class="headings">
-      <th  width="15%"  class="title"><?php echo __('Order Numer') ?></th>
-      <th  width="20%" class="title"><?php echo __('Date') ?></th>
-      <th  width="55%" class="title"><?php echo __('Description') ?></th>
-      <th width="10%" class="title"  align="right" style="text-align: right;"><?php echo __('Amount') ?></th>
-    </tr>
-                <?php 
-                $amount_total = 0;
-                $incrment=1;
-                foreach($transactions as $transaction): ?>
-
-                 <?php
-                  if($incrment%2==0){
-                   $class= 'class="even"';
-                  }else{
+       <tr class="headings">
+          <th  width="15%"  class="title"><?php echo __('Order Numer') ?></th>
+          <th  width="20%" class="title"><?php echo __('Date') ?></th>
+          <th  width="55%" class="title"><?php echo __('Description') ?></th>
+          <th width="10%" class="title"  align="right" style="padding-right: 30px;"><?php echo __('Amount') ?></th>
+        </tr>
+        <?php 
+            $amount_total = 0;
+            $incrment=1;
+            foreach($transactions as $transaction):
+                if($incrment%2==0){
+                    $class= 'class="even"';
+                }else{
                     $class= 'class="odd"';
-                      }
- $incrment++;
-                  ?>
-                <tr <?php echo $class;?>>
-                  <td><?php  echo $transaction->getOrderId() ?></td>
-                  <td><?php echo  $transaction->getCreatedAt('d-m-Y') ?></td>
-                  <td><?php echo $transaction->getDescription() ?></td>
-                  <td align="right"><?php echo  number_format($transaction->getAmount(),2);  $amount_total += $transaction->getAmount() ?>
-                            <?php 
-//                            if($lang=="pl"){
-//                                echo ('plz');
-//                            }else if($lang=="en"){
-//                                echo ('eur');
-//                            }else{
-                                echo (sfConfig::get('app_currency_code'));
-//                            } ?></td>
-                
-                </tr>
-                <?php endforeach; ?>
-                <?php if(count($transactions)==0): ?>
-                <tr>
-                	<td colspan="4"><p><?php echo __('There are currently no transactions to show.') ?></p></td>
-                </tr>
-                <?php else: ?>
-                <tr>
-                	<td align="right" colspan="3"><strong>Total</strong></td>
-                	<td  align="right"><?php echo  number_format($amount_total,2) ?>
-                            <?php 
-//                            if($lang=="pl"){
-//                                echo ('plz');
-//                            }else if($lang=="en"){
-//                                echo ('eur');
-//                            }else{
-                                echo (sfConfig::get('app_currency_code'));
-//                            } ?></td>
-                </tr>	
-                <?php endif; ?>
-              </table>
+                }
+                $incrment++;
+        ?>
+        <tr <?php echo $class;?>>
+          <td><?php  echo $transaction->getOrderId() ?></td>
+          <td><?php echo  $transaction->getCreatedAt('d-m-Y') ?></td>
+          <td><?php echo $transaction->getDescription() ?></td>
+          <td align="right;" style="padding-right: 30px;">
+              <?php
+                    echo  number_format($transaction->getAmount(),2);  $amount_total += $transaction->getAmount();
+                    echo (sfConfig::get('app_currency_code'));
+              ?>
+          </td>
+       </tr>
+       <?php endforeach; ?>
+       <?php if(count($transactions)==0): ?>
+       <tr>
+          <td colspan="4"><p><?php echo __('There are currently no transactions to show.') ?></p></td>
+       </tr>
+       <?php else: ?>
+       <tr>
+          <td align="right" colspan="3"><strong>Total</strong></td>
+          <td  align="right" style="padding-right: 30px;">
+             <?php
+                echo  number_format($amount_total,2);
+                echo (sfConfig::get('app_currency_code'));
+            ?>
+          </td>
+        </tr>	
+        <?php endif; ?>
+     </table>
 </div>         
