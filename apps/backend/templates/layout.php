9f9c07d30188eef1010803913a2d9670319f4d84
Rubab Rafique <rr@zerocall.com>
2012-08-02 10:48:41 +0500

    usage

--- apps/backend/templates/layout.php
+++ apps/backend/templates/layout.php
@@ -603,10 +603,10 @@ jQuery(function(){
 
 
       if(jQuery( "#startdate" ).length > 0){
-         jQuery( "#startdate" ).datepicker({ minDate: '-2m +0w',maxDate: '0m +0w', dateFormat: 'yy-mm-dd' }); 
+         jQuery( "#startdate" ).datepicker({ dateFormat: 'yy-mm-dd' }); 
       }  
       if(jQuery( "#enddate" ).length > 0){
-         jQuery( "#enddate" ).datepicker({ minDate: '-2m +0w',maxDate: '0m +0w', dateFormat: 'yy-mm-dd'});
+         jQuery( "#enddate" ).datepicker({ dateFormat: 'yy-mm-dd'});
       }
       if(jQuery( "#stdate" ).length > 0){
          jQuery( "#stdate" ).datepicker({maxDate: '0m +0w', dateFormat: 'yy-mm-dd' });
