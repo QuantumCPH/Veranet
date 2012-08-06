07bcbad4b37cba7b03e5ddafd7661a9a4ba26bb3
Fatima Umer <fu@zerocall.com>
2012-08-06 11:47:00 +0500

    payment gateway integration

--- apps/b2c/modules/customer/templates/refilTransactionSuccess.php
+++ apps/b2c/modules/customer/templates/refilTransactionSuccess.php
@@ -70,8 +70,8 @@
 <!--                <input type="hidden" name="return_url" value="http://veranet.zerocall.com/" />-->
                 <input type="hidden" name="cancel_url" value="http://veranet.zerocall.com/b2c.php">
 <!--                <input type="hidden" name="cancel_url" value="<?php echo $cancel_url;?>" />
-                <input type="hidden" name="return_url" value="<?php echo $return_url;?>" />-->
-                <input type="hidden" name="status_url" value="fu@zerocall.com" />
+                <input type="hidden" name="return_url" value="<?php echo $return_url;?>" />
+                <input type="hidden" name="status_url" value="fu@zerocall.com" />-->
                 <?php if($customerBalance+$order->getExtraRefill() < 250){ ?>
                 <div style="margin-top:40px;">
                     <input type="submit" class="butonsigninsmall" name="button" style="width:101px;cursor: pointer;float: left; margin-left: 1px !important; margin-top: -5px;"  value="<?php echo __('Pay') ?>" />
