95778f6a7a686340a878c4f4cb93d177d6968a16
Rubab Rafique <rr@zerocall.com>
2012-08-06 15:09:59 +0500

    call history

--- apps/backend/modules/company/templates/usageSuccess.php
+++ apps/backend/modules/company/templates/usageSuccess.php
@@ -149,9 +149,9 @@
          $fromdate = date('Y-m-d 21:00:00', strtotime("-1 day",strtotime($fromdate)));
          $todate = date('Y-m-d 21:59:59', strtotime($todate));
          
-//          echo    $fromdate;
-//          echo '<br />';
-//          echo    $todate;   
+          echo    $fromdate;
+          echo '<br />';
+          echo    $todate;   
          if(isset($empl)){
            $tilentaSubResult = CompanyEmployeActivation::getSubscription($empl, $fromdate, $todate);
           // var_dump($tilentaSubResult);
